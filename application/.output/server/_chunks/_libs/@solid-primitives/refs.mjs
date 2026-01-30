38ed13b161fb205561fbc93f8425a638c876d0f4
Rajveer Singh <therajveersingh@icloud.com>
2026-01-30 15:37:44 +0530

    chore: remove cached build artifacts and database file

--- application/.output/server/_chunks/_libs/@solid-primitives/refs.mjs
+++
@@ -1,7 +0,0 @@
-import { c as chain } from "./utils.mjs";
-function mergeRefs(...refs) {
-  return chain(refs);
-}
-export {
-  mergeRefs as m
-};
--- application/.output/server/_chunks/_libs/@solid-primitives/utils.mjs
+++
@@ -1,9 +0,0 @@
-function chain(callbacks) {
-  return (...args) => {
-    for (const callback of callbacks)
-      callback && callback(...args);
-  };
-}
-export {
-  chain as c
-};
